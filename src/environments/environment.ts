00389a65484bd1a0bdf439ec24a4474f0531ae55
Henderson Hood <hendy@hendersonhood.com>
2025-05-14 12:58:35 -0400

    more api key errors

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: 'AIzaSyArH242KXibjWarhrkgw9XQZyeuWupCEHY',
-    authDomain: 'friendlychat-bba62.firebaseapp.com',
-    databaseURL: 'https://friendlychat-bba62.firebaseio.com',
-    projectId: 'friendlychat-bba62',
-    storageBucket: 'friendlychat-bba62.appspot.com',
-    messagingSenderId: '92416365237',
-    appId: '1:92416365237:web:56fdc8307712936424a4fc',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: "AIzaSyArH242KXibjWarhrkgw9XQZyeuWupCEHY",
+    authDomain: "friendlychat-bba62.firebaseapp.com",
+    databaseURL: "https://friendlychat-bba62.firebaseio.com",
+    projectId: "friendlychat-bba62",
+    storageBucket: "friendlychat-bba62.appspot.com",
+    messagingSenderId: "92416365237",
+    appId: "1:92416365237:web:56fdc8307712936424a4fc",
   },
 };
 
