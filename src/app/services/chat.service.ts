9c7b23014c0d06ee2a8309897bf78f1079528cf7
Henderson Hood <hendy@hendersonhood.com>
2025-05-15 00:29:12 -0400

    call  requestNotificationsPermissions() after login

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -79,6 +79,7 @@ export class ChatService {
     signInWithPopup(this.auth, this.provider).then((result) => {
         const credential = GoogleAuthProvider.credentialFromResult(result);
         this.router.navigate(['/', 'chat']);
+        this.requestNotificationsPermissions(); // Request notification permissions after login
         return credential;
     })
   }
